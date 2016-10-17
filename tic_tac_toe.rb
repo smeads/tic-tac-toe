3f7d573706ceae97802872be8077fa0b0591f716
Steven Meads <sbmeads@gmail.com>
2016-10-17 08:22:53 -0700

    Add minitest for tie & game over

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -100,5 +100,5 @@ class Game
 
 end
 
-game = Game.new
-game.start_game
+# game = Game.new
+# game.start_game
--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -1,50 +1,31 @@
 gem 'minitest', '~> 5.4'
 gem 'minitest-reporters'
 require 'minitest/autorun'
+require_relative 'tic_tac_toe'
 require 'minitest/reporters'
 Minitest::Reporters.use!
-#require_relative 'tic_tac_toe'
 
-class Game < Minitest::Test
-  def test_print_board
-    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    expected = "#{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n" +
-      "Enter [0-8]:"
-    assert_equal expected, ::Game.new.start_game
+class TestGame < Minitest::Test
+
+  # def test_get_human_spot
+  #   expected = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+  #   assert_equal expected, ::Game.new.get_human_spot
+  # end
+
+  def test_not_tie
+    assert_equal false, ::Game.new.tie(["O", "X", "O", "X", "0", "X", "X", "O", "X"])
   end
-#
-#   def test_until_game_over_or_tied
-#     expected = until game_is_over(@board) || tie(@board)
-#       get_human_spot
-#       puts "#{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
-#     end
-#     assert_equal expected, ::Game.new.start_game
-#   end
-#
-#   def test_if_game_not_over_not_tied
-#     expected = if !game_is_over(@board) && !tie(@board)
-#       eval_board
-#     end
-#     assert_equal expected, ::Game.new.start_game
-#   end
-#
-#   def test_game_over
-#     expected = "Game over"
-#     assert_equal expected, ::Game.new.start_game
-#   end
-#
-#   def
-# end
 
-# describe Game do
-#   before do
-#     @game = Game.new
-#   end
-#
-#   describe "when user starts game" do
-#     it "prints the board" do
-#       @game.start_game.must_equal "#{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
-#     end
-#   end
-#
-# end
+  def test_tie
+    assert_equal true, ::Game.new.tie(["O", "X", "O", "X", "O", "X", "X", "O", "X"])
+  end
+
+  def test_game_is_over
+    assert_equal true, ::Game.new.game_is_over(["0", "0", "0"])
+  end
+
+  def test_game_is_not_over
+    assert_equal false, ::Game.new.game_is_over(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
+  end
+
+end
